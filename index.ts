c469f55812dbc9b994c726d69efc4b66779b660a
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-14 21:21:22 +0300

    fin

--- index.ts
+++ index.ts
@@ -4,9 +4,29 @@ import { generate } from 'rxjs';
 // https://www.learnrxjs.io/learn-rxjs/operators/creation/generate
 
 // generate sequences
-generate(
-  2,
-  (x) => x <= 8,
-  (x) => x + 3,
-  (x) => x * x
-).subscribe(console.log);
+
+// ex1
+// generate(
+//   2,
+//   (x) => x <= 8,
+//   (x) => x + 3,
+//   (x) => x * x
+// ).subscribe(console.log);
+
+// ex2
+// generate(
+//   2,
+//   (x) => x <= 8,
+//   (x) => x + 3
+// ).subscribe(console.log);
+
+const src$ = generate(
+  1, // Initial state
+  (value) => value <= 10, // Condition
+  (value) => value + 2, // Iterator
+  (value) => value * value // Result selector
+);
+
+src$.subscribe((el) => {
+  console.log(el);
+});
