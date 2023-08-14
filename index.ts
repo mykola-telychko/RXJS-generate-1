a85cba58d084ec4690261f41a7bb525c80884c5d
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-14 21:16:02 +0300

    fin

--- index.ts
+++ index.ts
@@ -1,9 +1,13 @@
 console.clear();
 import { generate } from 'rxjs';
 
-/
+// https://www.learnrxjs.io/learn-rxjs/operators/creation/generate
+
+// generate sequences
 generate(
   2,
-  x => x <= 8,
-  x => x + 3
+  (x) => x <= 8,
+  (x) => x + 3,
+  (x) => x * x,
+  3
 ).subscribe(console.log);
